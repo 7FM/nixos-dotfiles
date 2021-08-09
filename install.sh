1c97dfd6c18485930a4f8728ba0cc4c0c81fed31
7FM <41307817+7FM@users.noreply.github.com>
2021-08-10 00:09:35 +0200

    extend install script. ONLY use as reference

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env sh
-#ln -s $(pwd)/home.nix ~/.config/nixpkgs/home.nix
+
+# Quit on error
+set -e
+
+# Add home manager channel 21.05
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz home-manager
+nix-channel --update
+# Then: On NixOs you have to log out and sign back in
+
+#TODO is there a work around?
+
+nix-shell '<home-manager>' -A install
+
 rm -rf ~/.config/nixpkgs
 ln -s $(pwd)/home-manager ~/.config/nixpkgs
 
