5a8321eee1c1ab5eabf265ac7dc4282c9c7d3095
7FM <41307817+7FM@users.noreply.github.com>
2021-08-13 16:53:44 +0200

    make usbguard to allow all devices by default, BUT add a warning to change this when configured properly

--- nixos/common/security.nix
+++ nixos/common/security.nix
@@ -4,19 +4,34 @@ let
   runHeadless = config.custom.gui == "headless";
 in {
 
-  # Security
-  services.usbguard.enable = true;
-  # For headless:
-  services.usbguard.package = if runHeadless then pkgs.usbguard-nox else pkgs.usbguard;
+  config = {
+    # Security
+    services.usbguard.enable = true;
 
-  # Allow unfree software :(
-  nixpkgs.config.allowUnfree = true;
+    # TODO change back to "block" once a policy is inplace!
+    services.usbguard.implictPolicyTarget = "allow";
 
-  # Enfore nixstore to be readonly
-  nix.readOnlyStore = true;
+    # For headless:
+    services.usbguard.package = if runHeadless then pkgs.usbguard-nox else pkgs.usbguard;
 
-  # User settings as i.e. the password may not be changed! Also no new users or groups can be added!
-  users.mutableUsers = false;
+    # Allow unfree software :(
+    nixpkgs.config.allowUnfree = true;
+
+    # Enfore nixstore to be readonly
+    nix.readOnlyStore = true;
+
+    # User settings as i.e. the password may not be changed! Also no new users or groups can be added!
+    users.mutableUsers = false;
+
+    warnings = if (config.services.usbguard.implictPolicyTarget != "block") then [
+      ''
+        The fallback policy of usbguard should be set to block! Else no additional security is gained!
+        HOWEVER, ensure that usbguard is setup with a policy before setting the fallback to block,
+        else there might be no way to interact with the system!
+      ''
+    ] else [ ];
+
+  };
 
 }
 
