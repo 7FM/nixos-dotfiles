4243fae40ef717ba9d70af85eeb1a6860dbae78d
7FM <41307817+7FM@users.noreply.github.com>
2021-08-15 16:39:54 +0200

    make systemInstall script somewhat useable

--- systemInstall.sh
+++ systemInstall.sh
@@ -3,17 +3,22 @@
 # Taken from: https://mt-caret.github.io/blog/posts/2020-06-29-optin-state.html
 
 # Create boot partition /dev/sdaZ
+BOOT_PART="/dev/sda1"
 # Create swap partition /dev/sdaY
+SWAP_PART="/dev/sda3"
 # Create empty system partition /dev/sdaX
+SYS_PART="/dev/sda2"
 # Then:
 
 nix-env -iA nixos.htop
 nix-env -iA nixos.git-crypt
 
-#sudo swapon /dev/sdaY
+sudo swapon $SWAP_PART
 
-#sudo cryptsetup luksFormat /dev/sdaX
-#sudo cryptsetup open /dev/sdaX luks
+sudo cryptsetup luksFormat $SYS_PART
+sudo cryptsetup open $SYS_PART luks
+
+sudo mkfs.btrfs /dev/mapper/luks
 
 sudo mount -t btrfs /dev/mapper/luks /mnt
 sudo btrfs subvolume create /mnt/root
@@ -30,7 +35,7 @@ sudo mkdir /mnt/nix
 sudo mkdir -p /mnt/var/log
 sudo mkdir /mnt/boot
 
-#sudo mount /dev/sdaZ /mnt/boot/
+sudo mount $BOOT_PART /mnt/boot/
 sudo mount -o subvol=home,compress=zstd,noatime /dev/mapper/luks /mnt/home/
 sudo mount -o subvol=nix,compress=zstd,noatime /dev/mapper/luks /mnt/nix/
 sudo mount -o subvol=log,compress=zstd,noatime /dev/mapper/luks /mnt/var/log/
