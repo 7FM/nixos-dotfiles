07a6b1beec676501061b1faff7492073128ffafd
7FM <41307817+7FM@users.noreply.github.com>
2021-08-28 12:32:40 +0200

    add cli filebrowser with some utils

--- home-manager/modules/collections/utilities.nix
+++ home-manager/modules/collections/utilities.nix
@@ -8,6 +8,10 @@
     gparted
     zip
     unzip
+    sxiv # Image viewer
+    nnn # CLI file browser
+    moc # CLI audio player
+    unixtools.xxd # hexeditor
     #nmap
     nmap-graphical
     x2goclient
@@ -19,4 +23,11 @@
 
   # Config for htop
   home.file.".config/htop".source = ../../configs/htop;
+
+  # Plugins for nnn
+  home.file.".config/nnn/plugins".source = ../../configs/nnn/plugins;
+  # Enable certain plugins
+  home.sessionVariables = {
+    NNN_PLUG = "v:imgview;q:mocq;e:suedit;c:rsynccp;x:!chmod +x $nnn;h:hexview";
+  };
 }
