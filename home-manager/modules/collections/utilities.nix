dd4aafd10a729f317d5d8e3b0f1b90cc8f2e972e
7FM <41307817+7FM@users.noreply.github.com>
2021-09-22 18:53:56 +0200

    more utils

--- home-manager/modules/collections/utilities.nix
+++ home-manager/modules/collections/utilities.nix
@@ -5,6 +5,7 @@
     # Utilities
     wget
     htop
+    screen
     gparted
     zip
     unzip
@@ -19,6 +20,7 @@
     speedtest-cli
     usbutils
     pciutils
+    inetutils
     # Autoload shell.nix files
     direnv
   ];
