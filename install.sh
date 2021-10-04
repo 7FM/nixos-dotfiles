f7a86829ec4211c807fec22bd175396044594c38
7FM <41307817+7FM@users.noreply.github.com>
2021-10-04 18:11:03 +0200

    add script to switch to unstable channels

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ set -e
 
 # Add home manager channel 21.05
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz home-manager
+# Or use unstable
+#nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 # Then: On NixOs you have to log out and sign back in
 
---
+++ switchToUnstable.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+# system
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+
+./upgrade.sh
