de1ccedd1b6ce0cc278e0c7cb81c957abb66962f
7FM <41307817+7FM@users.noreply.github.com>
2021-10-12 18:26:41 +0200

    improved upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+set -e
+
+# Update channels
+echo "Update nix channels!"
 nix-channel --update
 sudo nix-channel --update
+
+# Upgrade packages
+echo "Upgrade packages!"
 home-manager switch
 sudo nixos-rebuild switch
