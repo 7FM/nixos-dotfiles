0985149e94b67ee263980d2f6885e00d1a172a7f
7FM <41307817+7FM@users.noreply.github.com>
2021-10-17 16:47:08 +0200

    add freecad helper macros

--- home-manager/modules/collections/diyStuff.nix
+++ home-manager/modules/collections/diyStuff.nix
@@ -9,6 +9,12 @@
     kicad
   ];
 
+  # Cura base configuration: you will need to copy these folders to the current version
+  # If we would simlink this directly to an version number then no changes are possible, which we want for slicing!
   home.file.".config/cura/baseSettings".source = ../../configs/cura/config;
   home.file.".local/share/cura/baseSettings".source = ../../configs/cura/local;
+
+  # FreeCAD helper macros
+  home.file.".FreeCAD/Macro/rotate_and_duplicate.FCMacro".source = ../../configs/freecad/Macro/rotate_and_duplicate.FCMacro;
+  home.file.".FreeCAD/Macro/spreadsheet_alias_to_left.FCMacro".source = ../../configs/freecad/Macro/spreadsheet_alias_to_left.FCMacro;
 }
