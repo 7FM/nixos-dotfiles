31f98e637b9a4d9ed97d08c796350e4391bcac39
7FM <41307817+7FM@users.noreply.github.com>
2021-11-12 01:08:53 +0100

    remove devel packages that are environment dependent anyway

--- home-manager/modules/collections/development.nix
+++ home-manager/modules/collections/development.nix
@@ -1,39 +1,14 @@
 { config, pkgs, lib, ... }:
 
 let
-  # Create an overlay to fix collisions between gcc & clang for bin/c{c,pp,++}
-  # For now default to gcc, we might want to change this in the future!
-  myClang = pkgs.clang.overrideAttrs (oldAttrs: {
-#    postInstall = (oldAttrs.postInstall or "") + ''
-#      rm $out/bin/cpp
-#      rm $out/bin/cc
-#      rm $out/bin/c++
-#    '';
-    installPhase = (oldAttrs.installPhase or "") + ''
-      rm $out/bin/cpp
-      rm $out/bin/cc
-      rm $out/bin/c++
-    '';
-  });
-
 in {
   imports = [
     ../vscode.nix
   ];
 
   home.packages = with pkgs; [
-    # Compiler & related
-    #python3Minimal
-    gcc # For now use gcc as default for the cpp binary
-#    clang
-    myClang # use the patched clang version
-    cmake
-    gnumake
-    gdb
     # IDEs
     jetbrains.idea-community
-    gtkwave
   ];
 
-  programs.java.enable = true;
 }
