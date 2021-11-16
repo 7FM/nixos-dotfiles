0b6526d62c99553e8f8fdb6adfd0b8b6d8c60710
7FM <41307817+7FM@users.noreply.github.com>
2021-11-16 01:06:25 +0100

    finally a somewhat working email setup!

--- home-manager/modules/email.nix
+++ home-manager/modules/email.nix
@@ -3,12 +3,25 @@
 let
   createPasswordLookupCmd = searchTerm: "secret-tool lookup email ${searchTerm}";
 
-  offlineimapConf = emailAddr: {
-    enable = true;
+  offlineimapConf = emailAddr: shortTag: customHook: hasIMAP: {
+    enable = hasIMAP;
     postSyncHookCommand = ''
       notmuch new
       notmuch tag -inbox +sent from:${emailAddr}
+      notmuch tag +${shortTag} to:${emailAddr}
+      ${customHook}
     '';
+  };
+
+  astroidConf = emailAddr: {
+    enable = true;
+    # sendMailCommand = ''
+    #   msmtpq --read-envelope-from --read-recipients --account=${emailAddr}
+    # '';
+  };
+
+  msmtpConf = emailAddr: hasSMTP: {
+    enable = hasSMTP;
   };
 
   notmuchConf = {
@@ -22,10 +35,33 @@ in {
       gnome.libsecret # Needed for secret-tool
     ];
 
-    programs.alot.enable = true;
-    programs.notmuch.enable = true;
-    programs.offlineimap.enable = true;
+    # Email frontend
+    # programs.alot.enable = true;
+    # TODO might need: GDK_BACKEND=x11
+    programs.astroid = {
+      enable = true;
+      pollScript = ''
+        # check if we have a connection
+        if ! ping -w 1 -W 1 -c 1 nixos.org; then
+            echo "there is no internet connection"
+            exit
+        fi
 
-    accounts.email.accounts = import ../configs/secrets/email/emailAddresses.nix { inherit createPasswordLookupCmd offlineimapConf notmuchConf; };
+        offlineimap
+      '';
+      extraConfig = {
+        # poll.interval = 0;
+        poll.interval = 180;
+        editor.attachment_words = "attach,anbei,anhang,angehängt";
+      };
+    };
+    # Email indexer
+    programs.notmuch.enable = true;
+    # Email fetcher
+    programs.offlineimap.enable = true;
+    # Email sender
+    programs.msmtp.enable = true;
+
+    accounts.email.accounts = import ../configs/secrets/email/emailAddresses.nix { inherit createPasswordLookupCmd offlineimapConf notmuchConf astroidConf msmtpConf; };
   };
 }
