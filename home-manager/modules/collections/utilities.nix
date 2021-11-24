bbc86b4eae99c4fcf68823a2bdbb70779ebac72c
7FM <41307817+7FM@users.noreply.github.com>
2021-11-24 18:33:08 +0100

    switch to nix-direnv

--- home-manager/modules/collections/utilities.nix
+++ home-manager/modules/collections/utilities.nix
@@ -18,13 +18,43 @@ in {
       usbutils
       pciutils
       inetutils
-      # Autoload shell.nix files
-      direnv
     ];
 
-    # Direnv shell integration: https://direnv.net/docs/hook.html
+    programs.direnv = {
+      enable = true;
+      enableZshIntegration = true;
+
+      nix-direnv.enable = true;
+    };
+
     programs.zsh.initExtra = ''
-      eval "$(direnv hook zsh)"
+nixify() {
+  if [ ! -e ./.envrc ]; then
+    echo "use nix" > .envrc
+    direnv allow
+  fi
+  if [[ ! -e shell.nix ]] && [[ ! -e default.nix ]]; then
+    cat > default.nix <<'EOF'
+with import <nixpkgs> {};
+mkShell {
+  nativeBuildInputs = [
+    bashInteractive
+  ];
+}
+EOF
+    ${EDITOR:-vim} default.nix
+  fi
+}
+
+flakifiy() {
+  if [ ! -e flake.nix ]; then
+    nix flake new -t github:nix-community/nix-direnv .
+  elif [ ! -e .envrc ]; then
+    echo "use flake" > .envrc
+    direnv allow
+  fi
+  ${EDITOR:-vim} flake.nix
+}
     '';
 
     # Config for htop
