70f43b76d648045cdc56738b475478eac35d636d
7FM <41307817+7FM@users.noreply.github.com>
2021-12-10 13:47:45 +0100

    adjust intellij hotkeys

--- home-manager/modules/collections/development.nix
+++ home-manager/modules/collections/development.nix
@@ -12,5 +12,10 @@ in {
       # IDEs
       jetbrains.idea-community
     ];
+
+    # Intellij keymap settings
+    xdg.configFile."JetBrains/IdeaIC2021.2/keymaps/VscodeLike.xml".source = ../../configs/JetBrains/IdeaIC2021.2/keymaps/VscodeLike.xml;
+    xdg.configFile."JetBrains/IdeaIC2021.2/options/linux/keymap.xml".source = ../../configs/JetBrains/IdeaIC2021.2/options/linux/keymap.xml;
+
   } (import ../submodule/vscode.nix { inherit config pkgs lib; })]);
 }
