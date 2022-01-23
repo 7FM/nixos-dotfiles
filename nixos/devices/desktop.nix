8d4e55cb05f21ee421eb68ff686c2acc261bed49
7FM <41307817+7FM@users.noreply.github.com>
2022-01-23 12:44:03 +0100

    use latest kernel for desktop

--- nixos/devices/desktop.nix
+++ nixos/devices/desktop.nix
@@ -9,6 +9,7 @@ in (lib.mkMerge [{
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.extraModulePackages = with config.boot.kernelPackages; [ 
     rtl8812au
   ];
