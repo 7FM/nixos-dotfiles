0c95a230055a9dd8b81da6e6c9ae52baaf7e3519
7FM <41307817+7FM@users.noreply.github.com>
2022-05-01 18:55:57 +0200

    enable driSupport32Bit only for x64 systems

--- nixos/modules/gpu.nix
+++ nixos/modules/gpu.nix
@@ -7,7 +7,7 @@
     enable = true;
     driSupport = true;
     # On 64-bit systems, if you want OpenGL for 32-bit programs such as in Wine, you should also set the following:
-    driSupport32Bit = true;
+    driSupport32Bit = pkgs.stdenv.isx86_64;
   };
 
 }
