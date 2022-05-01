0e0a141073d8cb07ea4197e232fd9dfeec5e5636
7FM <41307817+7FM@users.noreply.github.com>
2022-05-01 17:22:37 +0200

    ssh server: add option for static authorized_keys

--- nixos/modules/ssh.nix
+++ nixos/modules/ssh.nix
@@ -8,6 +8,7 @@ let
   _ports = cfg.ports;
   ports = if lib.types.path.check _ports then (import _ports) else _ports;
   myTools = pkgs.myTools { inherit config pkgs lib; };
+  authorizedKeys = cfg.authorizedKeys;
 in {
   options.custom.sshServer = with lib; {
     enable = mkOption {
@@ -25,6 +26,23 @@ in {
         Specifies the ports on which the ssh server should listen!
       '';
     };
+    authorizedKeys = mkOption {
+      type = types.listOf types.str;
+      default = [];
+      description = ''
+        A list of verbatim OpenSSH public keys that should be added to the
+        user's authorized keys. The keys are added to a file that the SSH
+        daemon reads in addition to the the user's authorized_keys file.
+        You can combine the <literal>keys</literal> and
+        <literal>keyFiles</literal> options.
+        Warning: If you are using <literal>NixOps</literal> then don't use this
+        option since it will replace the key required for deployment via ssh.
+      '';
+      example = [
+        "ssh-rsa AAAAB3NzaC1yc2etc/etc/etcjwrsh8e596z6J0l7 example@host"
+        "ssh-ed25519 AAAAC3NzaCetcetera/etceteraJZMfk3QPfQ foo@bar"
+      ];
+    };
   };
 
   config = {
@@ -33,6 +51,7 @@ in {
     services.openssh.ports = ports;
     services.openssh.permitRootLogin = "no";
     services.openssh.passwordAuthentication = false;
+    users.users."tm".openssh.authorizedKeys.keys = authorizedKeys;
 
     # Start ssh agent to manage the ssh keys
     programs.ssh.startAgent = !runHeadless;
