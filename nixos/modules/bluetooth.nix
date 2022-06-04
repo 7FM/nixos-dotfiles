c79feb214152573e8994547c5f6e1328ed9edbc8
7FM <41307817+7FM@users.noreply.github.com>
2022-06-04 12:31:42 +0200

    enable experimental bluez features

--- nixos/modules/bluetooth.nix
+++ nixos/modules/bluetooth.nix
@@ -9,6 +9,8 @@ in {
     hardware.bluetooth = {
       enable = true;
       powerOnBoot = false;
+      package = pkgs.bluez5-experimental;
+      settings.General.Experimental = true;
     };
   };
 
