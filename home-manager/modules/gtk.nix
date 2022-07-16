21a6f46406c0e180e5e0f24b70b8877f9fb408a2
7FM <41307817+7FM@users.noreply.github.com>
2022-07-16 20:55:06 +0200

    apply recommended sway gtk adwaita theme fix

--- home-manager/modules/gtk.nix
+++ home-manager/modules/gtk.nix
@@ -11,8 +11,28 @@ in {
 
     gtk = {
       enable = true;
-      gtk3.extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
+      gtk3 = {
+        extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+        };
+        # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland#removing-the-rounded-corners-and-dropshadow-of-themes-like-adwaita
+        extraCss = ''
+          /** Some apps use titlebar class and some window */
+          .titlebar,
+          window {
+            border-radius: 0;
+            box-shadow: none;
+          }
+
+          /** also remove shadows */
+          decoration {
+            box-shadow: none;
+          }
+
+          decoration:backdrop {
+            box-shadow: none;
+          }
+        '';
       };
     };
   };
