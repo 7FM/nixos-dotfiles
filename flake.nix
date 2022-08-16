e1f3315e44f5051520751612ca9ffe784d39fbc3
7FM <41307817+7FM@users.noreply.github.com>
2022-08-17 01:29:58 +0200

    patch klipper to use python3

--- flake.nix
+++ flake.nix
@@ -45,6 +45,55 @@
                   };
                   mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
                 });
+
+                klipper = prev.stdenv.mkDerivation rec {
+                  pname = "klipper";
+                  version = "unstable-2022-03-14";
+
+                  src = prev.fetchFromGitHub {
+                    owner = "KevinOConnor";
+                    repo = "klipper";
+                    rev = "30098db22a43274ceb87e078e603889f403a35c4";
+                    sha256 = "sha256-ORpXBFGPY6A/HEYX9Hhwb3wP2KcAE+z3pTxf6j7CwGg=";
+                  };
+
+                  sourceRoot = "source/klippy";
+
+                  # there is currently an attempt at moving it to Python 3, but it will remain
+                  # Python 2 for the foreseeable future.
+                  # c.f. https://github.com/KevinOConnor/klipper/pull/3278
+                  # NB: This is needed for the postBuild step
+                  nativeBuildInputs = [ (prev.python3.withPackages ( p: with p; [ cffi ] )) ];
+
+                  buildInputs = [ (prev.python3.withPackages (p: with p; [ cffi pyserial greenlet jinja2 numpy ])) ];
+
+                  # we need to run this to prebuild the chelper.
+                  postBuild = "python3 ./chelper/__init__.py";
+
+                  # NB: We don't move the main entry point into `/bin`, or even symlink it,
+                  # because it uses relative paths to find necessary modules. We could wrap but
+                  # this is used 99% of the time as a service, so it's not worth the effort.
+                  installPhase = let
+                    patchPythonInterpreter = f: "substituteInPlace ${f} --replace '#!/usr/bin/env python2' '#!/usr/bin/env python3'";
+                  in ''
+                    runHook preInstall
+
+                    ${patchPythonInterpreter "./klippy.py"}
+                    ${patchPythonInterpreter "./console.py"}
+                    ${patchPythonInterpreter "./parsedump.py"}
+
+                    mkdir -p $out/lib/klipper
+                    cp -r ./* $out/lib/klipper
+
+                    # Moonraker expects `config_examples` and `docs` to be available
+                    # under `klipper_path`
+                    cp -r $src/docs $out/lib/docs
+                    cp -r $src/config $out/lib/config
+
+                    chmod 755 $out/lib/klipper/klippy.py
+                    runHook postInstall
+                  '';
+                };
               })
             ];
 
