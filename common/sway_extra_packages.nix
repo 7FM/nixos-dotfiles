306f8295f9e11008281d686a52807e13c4d96bd0
7FM <41307817+7FM@users.noreply.github.com>
2023-01-07 20:28:12 +0100

    swaync needs to be in the path in order to work proberly... not sure why though

--- common/sway_extra_packages.nix
+++ common/sway_extra_packages.nix
@@ -8,4 +8,5 @@ with pkgs; [
   xorg.xlsclients # Helper program to show programs running using xwayland
   xorg.xhost # can be used to allow Xwayland applications to run as root, i.e. gparted
   wl-clipboard
+  swaynotificationcenter # Not sure why, but swaync needs to be explicitly installed. Otherwise 'COULD NOT FIND CSS FILE! REINSTALL THE PACKAGE!' is produced
 ]
