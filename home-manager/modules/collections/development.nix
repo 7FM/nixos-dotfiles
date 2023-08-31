3be68fe1e7b4b3b601b21a6385cd7979ca3c4298
7FM <41307817+7FM@users.noreply.github.com>
2023-08-31 11:44:08 +0200

    disable intellij for now

--- home-manager/modules/collections/development.nix
+++ home-manager/modules/collections/development.nix
@@ -2,14 +2,10 @@
 
 let
   enable = osConfig.custom.hm.collections.development.enable;
+  withIntellij = false;
 in {
-  config = lib.mkIf enable (lib.mkMerge [{
-    # imports = [
-    #   ../vscode.nix
-    # ];
-
+  config = lib.mkIf enable (lib.mkMerge [(lib.mkIf withIntellij {
     home.packages = with pkgs; [
-      # IDEs
       jetbrains.idea-community
     ];
 
@@ -17,5 +13,5 @@ in {
     xdg.configFile."JetBrains/IdeaIC2021.2/keymaps/VscodeLike.xml".source = ../../configs/JetBrains/IdeaIC2021.2/keymaps/VscodeLike.xml;
     xdg.configFile."JetBrains/IdeaIC2021.2/options/linux/keymap.xml".source = ../../configs/JetBrains/IdeaIC2021.2/options/linux/keymap.xml;
 
-  } (import ../submodule/vscode.nix { inherit config pkgs lib; })]);
+  }) (import ../submodule/vscode.nix { inherit config pkgs lib; })]);
 }
