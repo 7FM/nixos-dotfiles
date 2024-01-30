0fbc5ef5ef471aef0b8920c3659809b8442a0129
7FM <41307817+7FM@users.noreply.github.com>
2024-01-30 11:50:06 +0100

    tmserver: first draft to automate ddns & router updates

--- common/settings/tmserver.nix
+++ common/settings/tmserver.nix
@@ -254,11 +254,9 @@ in lib.mkMerge [
     };
   };
 
-  #TODO hard drive & mount configuration!
-
   # Backups
   services.btrbk = {
-    #TODO
+    #TODO backup configuration
   };
 
   # Server certificate: Let's Encrypt!
@@ -269,7 +267,7 @@ in lib.mkMerge [
   systemd.services."acme-${letsEncryptHost}" = let
     port = 80;
   in {
-    # TODO preStart and postStop were replaced by explicit ExecStartPre and ExecStopPort to run this as root user
+    # NOTE: preStart and postStop were replaced by explicit ExecStartPre and ExecStopPort to run this as root user
     # preStart = ''
     # postStop = ''
     serviceConfig = {
@@ -627,7 +625,399 @@ in lib.mkMerge [
   };
 
   # TODO DryNoMore
-  #TODO Compal shitty router scripting
+
+
+  # Scripted DDNS & Router firewall updates
+  systemd.timers."update-ddns" = {
+    wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnBootSec = "30m";
+        OnUnitActiveSec = "30m";
+        Unit = "update-ddns.service";
+      };
+  };
+
+  systemd.services."update-ddns" = let
+    myPython = pkgs.python3;
+    myPyPackages = python-packages: with python-packages; [
+      (callPackage ../../custom_pkgs/compal.nix { })
+    ];
+    myPythonWithPackages = myPython.withPackages myPyPackages;
+
+    update_router_forwards = let
+      portDefSet = (myTools.getSecret ../../nixos "usedPorts.nix") myTools;
+      exposedTCPPorts = myTools.getAllInternetExposedTCPports portDefSet;
+      exposedUDPPorts = myTools.getAllInternetExposedUDPports portDefSet;
+      createForwardRule = prot: ports: builtins.map 
+        (p: "    IPv6FilterRule(dir=RuleDir.incoming, dst_addr=newIPv6Addr, dst_sport=${toString p}, dst_eport=${toString p}, protocol=IPv6FilterRuleProto.${prot})")
+        ports;
+      forwardRules = builtins.concatStringsSep ",\n" ((createForwardRule "tcp" exposedTCPPorts) ++ (createForwardRule "udp" exposedUDPPorts));
+    in pkgs.writeText "update_router_forwards.py" ''
+      from compal import *
+      import sys
+
+      if len(sys.argv) < 3:
+        print("Missing argument: routerPwd ipAddr")
+        sys.exit(1)
+
+      routerPwd = sys.argv[1]
+      newIPv6Addr = sys.argv[2]
+
+      modem = Compal('192.168.0.1', routerPwd, False, True)
+      modem.login()
+
+      fw = Filters(modem)
+      inRules, outRules = fw.delete_all_ipv6_filter_rules()
+
+      newRules = [
+      ${forwardRules}
+      ]
+
+      # Add the updated rules
+      for rule in newRules:
+          fw.set_ipv6_filter_rule(rule)
+
+      # And logout
+      modem.logout()
+    '';
+
+      securityOptions = {
+        ProtectHome = true;
+        PrivateUsers = true;
+        PrivateDevices = true;
+        ProtectClock = true;
+        ProtectHostname = true;
+        ProtectProc = "invisible";
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectKernelLogs = true;
+        ProtectControlGroups = true;
+        RestrictNamespaces = true;
+        LockPersonality = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        MemoryDenyWriteExecute = true;
+        SystemCallArchitectures = "native";
+        RestrictAddressFamilies = [ "AF_INET" ];
+        # RestrictAddressFamilies = [ "AF_UNIX" "AF_INET" ];
+      };
+
+    # Hardcoded due to DynamicUser
+    update_ddns_root = "/var/lib/update_ddns";
+
+  in {
+    serviceConfig = securityOptions // {
+      Type = "oneshot";
+      #TODO unpriviledged
+      # User = "update_ddns";
+      # Group = "update_ddns";
+      DynamicUser = true;
+      StateDirectory = "update_ddns";
+      RuntimeDirectory = "update_ddns";
+      LogsDirectory = "update_ddns";
+      ConfigurationDirectory = "update_ddns";
+    };
+    script = ''
+#.Distributed under the terms of the GNU General Public License (GPL) version 2.0
+#
+# script for sending updates to no-ip.com / noip.com
+#.2014-2015 Christian Schoenebeck <christian dot schoenebeck at gmail dot com>
+#
+# This script is parsed by dynamic_dns_functions.sh inside send_update() function
+#
+# provider did not reactivate records, if no IP change was recognized
+# so we send a dummy (localhost) and a seconds later we send the correct IP addr
+#
+
+username=$(${pkgs.coreutils}/bin/cat ${update_ddns_root}/ddns_user)
+password=$(${pkgs.coreutils}/bin/cat ${update_ddns_root}/ddns_pwd)
+domain="${letsEncryptHost}"
+use_ipv6=1
+use_https=1
+force_ipversion=0
+retry_count=0
+USE_CURL=0
+
+# Extract the ipv6 address
+ipv6Prefix=$(ip -6 addr show dev eth0 | ${pkgs.gnugrep}/bin/grep -v 'mngtmpaddr' | ${pkgs.gnugrep}/bin/grep -v 'deprecated' | ${pkgs.gnused}/bin/sed -n 's|^.*inet6 \([^ ]*/64\).*global.*$|\1|p')
+__IP=''${ipv6Prefix%/??}
+
+VERBOSE=0		# default mode is log to console, but easily changed with parameter
+
+DATFILE="dat.file"		# save stdout data of WGet and other external programs called
+ERRFILE="err.file"		# save stderr output of WGet and other external programs called
+${pkgs.coreutils}/bin/rm -f "$DATFILE"
+${pkgs.coreutils}/bin/rm -f "$ERRFILE"
+
+RETRY_SECONDS=0		# in configuration
+
+URL_USER=""		# url encoded $username from config file
+URL_PASS=""		# url encoded $password from config file
+
+PID_SLEEP=0		# ProcessID of current background "sleep"
+
+# Transfer Programs
+WGET=${pkgs.wget}/bin/wget
+$WGET -V 2>/dev/null | ${pkgs.gnugrep}/bin/grep -F -q +https && WGET_SSL=$WGET
+
+CURL=${pkgs.curl}/bin/curl
+# CURL_SSL not empty then SSL support available
+CURL_SSL=$($CURL -V 2>/dev/null | ${pkgs.gnugrep}/bin/grep -F "https")
+
+# replace all special chars to their %hex value
+# used for USERNAME and PASSWORD in update_url
+# unchanged: "-"(minus) "_"(underscore) "."(dot) "~"(tilde)
+# to verify: "'"(single quote) '"'(double quote)	# because shell delimiter
+#            "$"(Dollar)				# because used as variable output
+# tested with the following string stored via Luci Application as password / username
+# A B!"#AA$1BB%&'()*+,-./:;<=>?@[\]^_`{|}~	without problems at Dollar or quotes
+urlencode() {
+	# $1	Name of Variable to store encoded string to
+	# $2	string to encode
+	local __ENC
+
+	[ $# -ne 2 ] && ${pkgs.coreutils}/bin/echo "Error calling 'urlencode()' - wrong number of parameters" && exit
+
+	__ENC="$(${pkgs.gawk}/bin/awk -v str="$2" 'BEGIN{ORS="";for(i=32;i<=127;i++)lookup[sprintf("%c",i)]=i
+		for(k=1;k<=length(str);++k){enc=substr(str,k,1);if(enc!~"[-_.~a-zA-Z0-9]")enc=sprintf("%%%02x", lookup[enc]);print enc}}')"
+
+	eval "$1=\"$__ENC\""	# transfer back to variable
+	return 0
+}
+
+do_transfer() {
+	# $1	# URL to use
+	local __URL="$1"
+	local __ERR=0
+	local __CNT=0	# error counter
+	local __PROG  __RUNPROG
+
+	[ $# -ne 1 ] && ${pkgs.coreutils}/bin/echo "Error in 'do_transfer()' - wrong number of parameters" && exit
+
+	# Use ip_network as default for bind_network if not separately specified
+	[ -z "$bind_network" ] && [ "$ip_source" = "network" ] && [ "$ip_network" ] && bind_network="$ip_network"
+
+	# lets prefer GNU Wget because it does all for us - IPv4/IPv6/HTTPS/PROXY/force IP version
+	if [ -n "$WGET_SSL" ] && [ $USE_CURL -eq 0 ]; then 			# except global option use_curl is set to "1"
+		__PROG="$WGET --hsts-file=/tmp/.wget-hsts -nv -t 1 -O $DATFILE -o $ERRFILE"	# non_verbose no_retry outfile errfile
+		# force network/ip to use for communication
+		if [ -n "$bind_network" ]; then
+			local __BINDIP
+			# set correct program to detect IP
+			[ $use_ipv6 -eq 0 ] && __RUNPROG="network_get_ipaddr" || __RUNPROG="network_get_ipaddr6"
+			eval "$__RUNPROG __BINDIP $bind_network" || \
+				${pkgs.coreutils}/bin/echo "Can not detect local IP using '$__RUNPROG $bind_network' - Error: '$?'" && exit
+			${pkgs.coreutils}/bin/echo "Force communication via IP '$__BINDIP'"
+			__PROG="$__PROG --bind-address=$__BINDIP"
+		fi
+		# force ip version to use
+		if [ $force_ipversion -eq 1 ]; then
+			[ $use_ipv6 -eq 0 ] && __PROG="$__PROG -4" || __PROG="$__PROG -6"	# force IPv4/IPv6
+		fi
+		# set certificate parameters
+		if [ $use_https -eq 1 ]; then
+			if [ "$cacert" = "IGNORE" ]; then	# idea from Ticket #15327 to ignore server cert
+				__PROG="$__PROG --no-check-certificate"
+			elif [ -f "$cacert" ]; then
+				__PROG="$__PROG --ca-certificate=''${cacert}"
+			elif [ -d "$cacert" ]; then
+				__PROG="$__PROG --ca-directory=''${cacert}"
+			elif [ -n "$cacert" ]; then		# it's not a file and not a directory but given
+				${pkgs.coreutils}/bin/echo "No valid certificate(s) found at '$cacert' for HTTPS communication" && exit
+			fi
+		fi
+		# disable proxy if no set (there might be .wgetrc or .curlrc or wrong environment set)
+		[ -z "$proxy" ] && __PROG="$__PROG --no-proxy"
+
+		# user agent string if provided
+		if [ -n "$user_agent" ]; then
+			# replace single and double quotes
+			user_agent=$(${pkgs.coreutils}/bin/echo $user_agent | ${pkgs.gnused}/bin/sed "s/'/ /g" | ${pkgs.gnused}/bin/sed 's/"/ /g')
+			__PROG="$__PROG --user-agent='$user_agent'"
+		fi
+
+		__RUNPROG="$__PROG '$__URL'"	# build final command
+		__PROG="GNU Wget"		# reuse for error logging
+
+	# 2nd choice is cURL IPv4/IPv6/HTTPS
+	# libcurl might be compiled without Proxy or HTTPS Support
+	elif [ -n "$CURL" ]; then
+		__PROG="$CURL -RsS -o $DATFILE --stderr $ERRFILE"
+		# check HTTPS support
+		[ -z "$CURL_SSL" -a $use_https -eq 1 ] && \
+			${pkgs.coreutils}/bin/echo "cURL: libcurl compiled without https support" && exit
+		# force network/interface-device to use for communication
+		if [ -n "$bind_network" ]; then
+			local __DEVICE
+			network_get_device __DEVICE $bind_network || \
+				${pkgs.coreutils}/bin/echo "Can not detect local device using 'network_get_device $bind_network' - Error: '$?'" && exit
+			${pkgs.coreutils}/bin/echo "Force communication via device '$__DEVICE'"
+			__PROG="$__PROG --interface $__DEVICE"
+		fi
+		# force ip version to use
+		if [ $force_ipversion -eq 1 ]; then
+			[ $use_ipv6 -eq 0 ] && __PROG="$__PROG -4" || __PROG="$__PROG -6"	# force IPv4/IPv6
+		fi
+		# set certificate parameters
+		if [ $use_https -eq 1 ]; then
+			if [ "$cacert" = "IGNORE" ]; then	# idea from Ticket #15327 to ignore server cert
+				__PROG="$__PROG --insecure"	# but not empty better to use "IGNORE"
+			elif [ -f "$cacert" ]; then
+				__PROG="$__PROG --cacert $cacert"
+			elif [ -d "$cacert" ]; then
+				__PROG="$__PROG --capath $cacert"
+			elif [ -n "$cacert" ]; then		# it's not a file and not a directory but given
+				${pkgs.coreutils}/bin/echo "No valid certificate(s) found at '$cacert' for HTTPS communication" && exit
+			fi
+		fi
+		# disable proxy if no set (there might be .wgetrc or .curlrc or wrong environment set)
+		# or check if libcurl compiled with proxy support
+		if [ -z "$proxy" ]; then
+			__PROG="$__PROG --noproxy '*'"
+		elif [ -z "$CURL_PROXY" ]; then
+			# if libcurl has no proxy support and proxy should be used then force ERROR
+			${pkgs.coreutils}/bin/echo "cURL: libcurl compiled without Proxy support" && exit
+		fi
+
+		__RUNPROG="$__PROG '$__URL'"	# build final command
+		__PROG="cURL"			# reuse for error logging
+
+	# uclient-fetch possibly with ssl support if /lib/libustream-ssl.so installed
+	elif [ -n "$UCLIENT_FETCH" ]; then
+		# UCLIENT_FETCH_SSL not empty then SSL support available
+		UCLIENT_FETCH_SSL=$(find /lib /usr/lib -name libustream-ssl.so* 2>/dev/null)
+		__PROG="$UCLIENT_FETCH -q -O $DATFILE"
+		# force network/ip not supported
+		[ -n "$__BINDIP" ] && \
+			${pkgs.coreutils}/bin/echo "uclient-fetch: FORCE binding to specific address not supported" && exit
+		# force ip version to use
+		if [ $force_ipversion -eq 1 ]; then
+			[ $use_ipv6 -eq 0 ] && __PROG="$__PROG -4" || __PROG="$__PROG -6"       # force IPv4/IPv6
+		fi
+		# https possibly not supported
+		[ $use_https -eq 1 -a -z "$UCLIENT_FETCH_SSL" ] && \
+			${pkgs.coreutils}/bin/echo "uclient-fetch: no HTTPS support! Additional install one of ustream-ssl packages" && exit
+		# proxy support
+		[ -z "$proxy" ] && __PROG="$__PROG -Y off" || __PROG="$__PROG -Y on"
+		# https & certificates
+		if [ $use_https -eq 1 ]; then
+			if [ "$cacert" = "IGNORE" ]; then
+				__PROG="$__PROG --no-check-certificate"
+			elif [ -f "$cacert" ]; then
+				__PROG="$__PROG --ca-certificate=$cacert"
+			elif [ -n "$cacert" ]; then		# it's not a file; nothing else supported
+				${pkgs.coreutils}/bin/echo "No valid certificate file '$cacert' for HTTPS communication" && exit
+			fi
+		fi
+		__RUNPROG="$__PROG '$__URL' 2>$ERRFILE"		# build final command
+		__PROG="uclient-fetch"				# reuse for error logging
+
+	# Busybox Wget or any other wget in search $PATH (did not support neither IPv6 nor HTTPS)
+	elif [ -n "$WGET" ]; then
+		__PROG="$WGET -q -O $DATFILE"
+		# force network/ip not supported
+		[ -n "$__BINDIP" ] && \
+			${pkgs.coreutils}/bin/echo "BusyBox Wget: FORCE binding to specific address not supported" && exit
+		# force ip version not supported
+		[ $force_ipversion -eq 1 ] && \
+			${pkgs.coreutils}/bin/echo "BusyBox Wget: Force connecting to IPv4 or IPv6 addresses not supported" && exit
+		# https not supported
+		[ $use_https -eq 1 ] && \
+			${pkgs.coreutils}/bin/echo "BusyBox Wget: no HTTPS support" && exit
+		# disable proxy if no set (there might be .wgetrc or .curlrc or wrong environment set)
+		[ -z "$proxy" ] && __PROG="$__PROG -Y off"
+
+		__RUNPROG="$__PROG '$__URL' 2>$ERRFILE"		# build final command
+		__PROG="Busybox Wget"				# reuse for error logging
+
+	else
+		${pkgs.coreutils}/bin/echo "Neither 'Wget' nor 'cURL' nor 'uclient-fetch' installed or executable" && exit
+	fi
+
+	while : ; do
+		${pkgs.coreutils}/bin/echo "#> $__RUNPROG"
+		eval $__RUNPROG			# DO transfer
+		__ERR=$?			# save error code
+		[ $__ERR -eq 0 ] && return 0	# no error leave
+
+		${pkgs.coreutils}/bin/echo "$__PROG Error: '$__ERR'"
+		${pkgs.coreutils}/bin/echo "$(${pkgs.coreutils}/bin/cat $ERRFILE)"		# report error
+
+		[ $VERBOSE -gt 1 ] && {
+			# VERBOSE > 1 then NO retry
+			${pkgs.coreutils}/bin/echo "Transfer failed - Verbose Mode: $VERBOSE - NO retry on error"
+			return 1
+		}
+
+		__CNT=$(( $__CNT + 1 ))	# increment error counter
+		# if error count > retry_count leave here
+		[ $retry_count -gt 0 -a $__CNT -gt $retry_count ] && \
+			${pkgs.coreutils}/bin/echo "Transfer failed after $retry_count retries" && return 1
+
+		${pkgs.coreutils}/bin/echo "Transfer failed - retry $__CNT/$retry_count in $RETRY_SECONDS seconds"
+		${pkgs.coreutils}/bin/sleep $RETRY_SECONDS &
+		PID_SLEEP=$!
+		wait $PID_SLEEP	# enable trap-handler
+		PID_SLEEP=0
+	done
+	# we should never come here there must be a programming error
+	${pkgs.coreutils}/bin/echo "Error in 'do_transfer()' - program coding error"
+}
+
+[ -n "$username" ] && urlencode URL_USER "$username"
+[ -n "$password" ] && urlencode URL_PASS "$password"
+
+#__DUMMY
+__UPDURL6="http://[USERNAME]:[PASSWORD]@dynupdate6.noip.com/nic/update?hostname=[DOMAIN]&myip=[IP]"
+__UPDURL="http://[USERNAME]:[PASSWORD]@dynupdate.noip.com/nic/update?hostname=[DOMAIN]&myip=[IP]"
+# inside url we need username and password
+[ -z "$URL_USER" ] && ${pkgs.coreutils}/bin/echo "Service section not configured correctly! Missing 'URL_USER'" && exit
+[ -z "$URL_PASS" ] && ${pkgs.coreutils}/bin/echo "Service section not configured correctly! Missing 'URL_PASS'" && exit
+[ -z "$__IP" ] && ${pkgs.coreutils}/bin/echo "Service section not configured correctly! Missing '__IP'" && exit
+[ -z "$domain" ] && ${pkgs.coreutils}/bin/echo "Service section not configured correctly! Missing 'domain'" && exit
+
+# set IP version dependend dummy (localhost)
+[ $use_ipv6 -eq 0 ] && __DUMMY="127.0.0.1" || __DUMMY="::1"
+[ $use_ipv6 -eq 0 ] && __UPDURL=$__UPDURL || __UPDURL=$__UPDURL6
+
+# lets do DUMMY transfer
+${pkgs.coreutils}/bin/echo "sending dummy IP to 'no-ip.com'"
+__URL=$(${pkgs.coreutils}/bin/echo $__UPDURL | ${pkgs.gnused}/bin/sed -e "s#\[USERNAME\]#$URL_USER#g" -e "s#\[PASSWORD\]#$URL_PASS#g" \
+			       -e "s#\[DOMAIN\]#$domain#g" -e "s#\[IP\]#$__DUMMY#g")
+[ $use_https -ne 0 ] && __URL=$(${pkgs.coreutils}/bin/echo $__URL | ${pkgs.gnused}/bin/sed -e 's#^http:#https:#')
+
+do_transfer "$__URL" || exit 1
+
+${pkgs.coreutils}/bin/echo "'no-ip.com' answered:''${N}$(${pkgs.coreutils}/bin/cat $DATFILE)"
+# analyse provider answers
+# "good [IP_ADR]"	= successful
+# "nochg [IP_ADR]"	= no change but OK
+${pkgs.gnugrep}/bin/grep -E "good|nochg" $DATFILE >/dev/null 2>&1 || exit 1
+
+# lets wait a seconds
+${pkgs.coreutils}/bin/sleep 1
+
+# now send the correct data
+${pkgs.coreutils}/bin/echo "sending real IP to 'no-ip.com'"
+__URL=$(${pkgs.coreutils}/bin/echo $__UPDURL | ${pkgs.gnused}/bin/sed -e "s#\[USERNAME\]#$URL_USER#g" -e "s#\[PASSWORD\]#$URL_PASS#g" \
+			       -e "s#\[DOMAIN\]#$domain#g" -e "s#\[IP\]#$__IP#g")
+[ $use_https -ne 0 ] && __URL=$(${pkgs.coreutils}/bin/echo $__URL | ${pkgs.gnused}/bin/sed -e 's#^http:#https:#')
+
+do_transfer "$__URL" || exit 1
+
+${pkgs.coreutils}/bin/echo "'no-ip.com' answered:''${N}$(${pkgs.coreutils}/bin/cat $DATFILE)"
+# analyse provider answers
+# "good [IP_ADR]"	= successful
+# "nochg [IP_ADR]"	= no change but OK
+${pkgs.gnugrep}/bin/grep -E "good|nochg" $DATFILE >/dev/null 2>&1 || exit 1
+
+${pkgs.coreutils}/bin/rm -f "$DATFILE"
+${pkgs.coreutils}/bin/rm -f "$ERRFILE"
+
+router_pwd=$(${pkgs.coreutils}/bin/cat ${update_ddns_root}/router_pwd)
+${myPythonWithPackages} ${update_router_forwards} "$router_pwd" "$__IP"
+    '';
+  };
 }
 (import (modulesPath + "/installer/scan/not-detected.nix") { inherit lib; })
 ]
---
+++ custom_pkgs/compal.nix
@@ -0,0 +1,50 @@
+{ lib
+, buildPythonPackage
+, fetchPypi
+, requests
+, lxml
+, pythonOlder
+, black
+, isort
+, responses
+, tox
+}:
+
+buildPythonPackage rec {
+  pname = "compal";
+  version = "0.5.1";
+
+  disabled = pythonOlder "3.7";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-2MaJrv+n1Q4Pu90NFfLg15Q9sIHHjJ5L9lugnLONR/Q=";
+  };
+
+  propagatedBuildInputs = [
+    requests
+    lxml
+  ];
+
+  nativeCheckInputs = [
+    black
+    isort
+    responses
+    tox
+  ];
+
+  # no tests are present
+  doCheck = true;
+
+  pythonImportsCheck = [ "compal" ];
+
+  meta = with lib; {
+    description = "Python interface for the Ziggo Connect Box/Compal CH7465LG";
+    longDescription = ''
+      This repository contains a simple api to wrap the web interface of the Ziggo Connect Box (i.e. the Compal CH7465LG).
+    '';
+    homepage = "https://github.com/ties/compal_CH7465LG_py";
+    license = with licenses; [ mit ];
+    #maintainers = with maintainers; [ ];
+  };
+}
