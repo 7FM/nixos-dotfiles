957b408771dcf23a657474797a204b96a1d30e91
7FM <41307817+7FM@users.noreply.github.com>
2024-03-13 22:28:03 +0100

    update calendar config

--- home-manager/configs/example_secrets/calendar/accounts.nix
+++ home-manager/configs/example_secrets/calendar/accounts.nix
@@ -11,7 +11,6 @@ let
 in {
   "Private" = (createCalendarAcc "Private" (ownCalDavServer "MyCalDavCalendarID") (khalConfig { color = "magenta"; })) // {
     primary = true;
-    primaryCollection = "Private";
   };
   "Bdays" = createCalendarAcc "Bdays" (ownCalDavServer "MyCalDavBDayCalendarID") (khalConfig { color = "dark green"; priority = 20; });
 }
