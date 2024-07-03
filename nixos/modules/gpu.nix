afe27fea6c8214595e83771d6a7f08145a8909ab
7FM <41307817+7FM@users.noreply.github.com>
2024-07-03 15:46:39 +0200

    opengl.driSupport is no longer needed

--- nixos/modules/gpu.nix
+++ nixos/modules/gpu.nix
@@ -7,7 +7,6 @@ in {
   # Basic hardware settings
   hardware.opengl = {
     enable = true;
-    driSupport = true;
     # On 64-bit systems, if you want OpenGL for 32-bit programs such as in Wine, you should also set the following:
     driSupport32Bit = pkgs.stdenv.isx86_64;
   };
