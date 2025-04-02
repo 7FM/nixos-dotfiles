b16ebd13704fa348ce99d58627d7f53423d0a1c5
7FM <41307817+7FM@users.noreply.github.com>
2025-04-03 00:00:39 +0200

    improve intel gpu support

--- nixos/modules/gpu_intel.nix
+++ nixos/modules/gpu_intel.nix
@@ -4,6 +4,16 @@ let
   myTools = pkgs.myTools { osConfig = config; };
   enable = config.custom.gpu == "intel";
 in {
-
+  hardware.graphics = {
+    extraPackages = with pkgs; [
+      vpl-gpu-rt
+      intel-media-driver # For Broadwell (2014) or newer processors. LIBVA_DRIVER_NAME=iHD
+      #intel-vaapi-driver # For older processors. LIBVA_DRIVER_NAME=i965
+    ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      intel-media-driver
+      #intel-vaapi-driver
+    ];
+  };
 }
 
