4b98a21b83279ee390bfae3c271a2390d04779e3
7FM <41307817+7FM@users.noreply.github.com>
2025-06-04 10:05:40 +0200

    Update scripts

--- home-manager/modules/scripts.nix
+++ home-manager/modules/scripts.nix
@@ -49,7 +49,7 @@ let
       sshuttle
     ];
     text = ''
-      sshuttle --dns -r esa_erebor 130.83.161.128/26 10.0.0.0/8 -x 130.83.161.131/32
+      sshuttle --dns -r esa_gateway 130.83.161.128/26 10.0.0.0/8 -x 130.83.161.131/32
     '';
   };
 
