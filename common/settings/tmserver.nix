43b4a1900d449ae124397a56cf955f8f9f209898
7FM <41307817+7FM@users.noreply.github.com>
2025-06-05 16:05:49 +0200

    tmserver: opencloud proxy increase timeouts

--- common/settings/tmserver.nix
+++ common/settings/tmserver.nix
@@ -645,9 +645,9 @@ in lib.mkMerge [
               proxy_set_header X-Real-IP $remote_addr;
               proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
               proxy_set_header X-Scheme $scheme;
-              proxy_connect_timeout   60s;
-              proxy_send_timeout      60s;
-              proxy_read_timeout      60s;
+              proxy_connect_timeout   3600s;
+              proxy_send_timeout      3600s;
+              proxy_read_timeout      3600s;
 
               client_max_body_size 0;
             '';
