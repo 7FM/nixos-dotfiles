5e3e2092e19673e479369fd27e32d85739be74d3
7FM <41307817+7FM@users.noreply.github.com>
2026-02-19 21:39:04 +0100

    laptop: gimme all performance on battery + charge earlier

--- nixos/modules/powermanagement.nix
+++ nixos/modules/powermanagement.nix
@@ -33,10 +33,11 @@ in {
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
+      CPU_MAX_PERF_ON_BAT = 100;
+      #CPU_MAX_PERF_ON_BAT = 20;
 
       #Optional helps save long term battery health
-      START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+      START_CHARGE_THRESH_BAT0 = 70; # 70 and bellow it starts to charge
       STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
     };
   };
